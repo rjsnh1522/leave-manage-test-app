2199ae8df04b20743caedfacdb9a71d9393fd670
logan <rjsnh1522@gmail.com>
2018-04-07 19:54:55 +0530

    users active admin columns

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -4,12 +4,20 @@ ActiveAdmin.register User do
 #
 # permit_params :list, :of, :attributes, :on, :model
 #
-# or
+ 
 #
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+index do 
+	column :id
+	column :email
+	column :created_at
+	column "Leave taken" ,:add_leave_count
+	actions
+end
 
 end
