2da94e9115ef9eee9de736558c4f894518d7e330
logan <rjsnh1522@gmail.com>
2018-04-07 19:05:55 +0530

    some design changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,11 @@ module ApplicationHelper
 	def leave_status(status)
 			case status
 				when 0
-				  "Applied"
+				  "<span class='label label-info'>Applied</span>"
 				when 1
-				  "Accepted"
+				  "<span class='label label-success'>Accepted</span>"
 				when 2
-				  "Rejected"
+				  "<span class='label label-danger'>Rejected</span>"
 			end
 
 	end
@@ -20,7 +20,7 @@ module ApplicationHelper
 
 	def leave_taken
 			if @user.present?
-				 total_leave = System.last.max_leave.present? ? System.last.max_leave : 12
+				 total_leave = System.last.try(:max_leave).present? ? System.last.max_leave : 12
 				 leaves_left = total_leave - @user.add_leave_count
 				 return leaves_left
 			end
